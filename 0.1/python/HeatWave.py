70101a8d04dfa5cc278b610be4c3823b57a21a30
Geshi San <43856847+GeshiSan@users.noreply.github.com>
2019-08-29 21:46:56 +0200

    Python ==> 0.1 done

---
+++ 0.1/python/HeatWave.py
@@ -0,0 +1,32 @@
+import threading, time, subprocess, os
+
+
+print(" __          ______ _______ _    ___          __")
+print(" \ \        / / __ \__   __| |  | \ \        / /")
+print("  \ \  /\  / / |  | | | |  | |__| |\ \  /\  / / ")
+print("   \ \/  \/ /| |  | | | |  |  __  | \ \/  \/ /  ")
+print("    \  /\  / | |__| | | |  | |  | |  \  /\  /   ")
+print("     \/  \/   \____/  |_|  |_|  |_|   \/  \/    ")
+print("#----------------------------------------------------------------------------------#")
+print("Welcome to the Python version of WOTHW ! This version is in bêta, so be indulgent.")
+print("Python version developped by GeshiSan")
+print("Propriety of BlackBotFR and GlitchBot, do not redistribute")
+print("#----------------------------------------------------------------------------------#")
+print("")
+print("Program is running ! In 3 hours you will be noticed.")
+
+def foo():
+    print("Starting Launch.vbs !")
+    filepath = 'launch.vbs'
+    os.startfile(filepath)
+    
+WAIT_TIME_SECONDS = 10800
+
+ticker = threading.Event()
+while not ticker.wait(WAIT_TIME_SECONDS):
+    foo()
+    
+#output:
+#Tue Oct 16 17:29:40 2018
+#Tue Oct 16 17:29:42 2018
+#Tue Oct 16 17:29:44 2018
