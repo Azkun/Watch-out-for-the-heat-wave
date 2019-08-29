1e5935faa39249f7c68ee5ad3a42fa9e917d5e45
Azkun <35082633+Azkun@users.noreply.github.com>
2019-08-29 19:57:30 +0200

    Update and rename NodeJs.js to HeatWaveNode.js

--- 0.1/HeatWaveNode.js
+++ 0.1/HeatWaveNode.js
@@ -2,5 +2,5 @@ const chalk = require('chalk');
 console.log(chalk.cyan('Welcome to the Js version ! This is a NodeJs ! Install NodeJs for this !  (If NodeJs is install, please with 3 hours'))
 
 setInterval(() => {
-//Indev
+console.log('Drink water !')
 }, 1.08e+7);
