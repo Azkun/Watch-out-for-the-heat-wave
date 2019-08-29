c9a4a4964a5b2334b98aba1ddcb5f943d839a6a4
Azkun <35082633+Azkun@users.noreply.github.com>
2019-08-29 18:04:09 +0200

    Add the Node programm (indev)

---
+++ 0.1/NodeJs.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+console.log('Welcome to the Js version ! This is a NodeJs ! Install NodeJs for this !  (If NodeJs is install, please with 3 hours')
+
+setInterval(() => {
+//Indev
+});
