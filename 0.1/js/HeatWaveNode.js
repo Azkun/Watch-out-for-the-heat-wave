3b00251139312d47e40fb1fef6393f96ccf30d3c
Azkun <35082633+Azkun@users.noreply.github.com>
2019-08-30 14:41:07 +0200

    Drink water has Chalked

--- 0.1/js/HeatWaveNode.js
+++ 0.1/js/HeatWaveNode.js
@@ -2,5 +2,5 @@ const chalk = require('chalk');
 console.log(chalk.cyan('Welcome to the Js version ! This is a NodeJs ! Install NodeJs for this !  (If NodeJs is install, please with 3 hours'))
 
 setInterval(() => {
-console.log('Drink water !')
+console.log(chalk.cyan('Drink water !'))
 }, 1.08e+7);
