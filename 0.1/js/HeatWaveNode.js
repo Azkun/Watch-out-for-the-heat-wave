ffd9b575bfee3da0512524526ab97b689bd67734
Azkun <35082633+Azkun@users.noreply.github.com>
2019-08-31 11:58:20 +0200

    Finishing HeatWaveNode.js (Windows)

--- 0.1/js/HeatWaveNode.js
+++ 0.1/js/HeatWaveNode.js
@@ -4,5 +4,8 @@ const chalk = require('chalk');
 console.log(chalk.cyan('Welcome to the Js version ! This is a NodeJs ! Install NodeJs for this !  (If NodeJs is install, please wait 3 hours'))
 
 setInterval(() => {
-console.log(chalk.cyan('Drink water (Indev)!'))
+console.log(chalk.cyan('Toulout ! (If nothing,  you are not on W10)'))
+const {exec} = require("child_process")
+exec('launch.vbs').unref()
 }, 1.08e+7);
+
