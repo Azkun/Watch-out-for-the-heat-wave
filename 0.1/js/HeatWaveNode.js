0a02b92e1bf99bb1f9a5743b4fdabd9c2f2ddb16
Azkun <35082633+Azkun@users.noreply.github.com>
2019-08-31 11:55:00 +0200

    Deleted spelling mistakes

--- 0.1/js/HeatWaveNode.js
+++ 0.1/js/HeatWaveNode.js
@@ -1,7 +1,7 @@
 
 //Open File is in dev
 const chalk = require('chalk');
-console.log(chalk.cyan('Welcome to the Js version ! This is a NodeJs ! Install NodeJs for this !  (If NodeJs is install, please with 3 hours'))
+console.log(chalk.cyan('Welcome to the Js version ! This is a NodeJs ! Install NodeJs for this !  (If NodeJs is install, please wait 3 hours'))
 
 setInterval(() => {
 console.log(chalk.cyan('Drink water (Indev)!'))
